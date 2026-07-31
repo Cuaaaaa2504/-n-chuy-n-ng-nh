b16956c1a9302c9da437739deec2a1ef7977981c
Minh Hanh <hanhchu2k5@gmail.com>
2026-08-01 00:05:00 +0700

    style: redesign light mode with blue neon theme

--- San Ve Frontend/San Ve Frontend/frontend1/src/main.tsx
+++ San Ve Frontend/San Ve Frontend/frontend1/src/main.tsx
@@ -8,6 +8,7 @@ import "./stitch-exact-v2.css";
 import "./soft-light-theme.css";
 import "./ui-fixes.css";
 import "./light-contrast.css";
+import "./light-neon-blue.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
